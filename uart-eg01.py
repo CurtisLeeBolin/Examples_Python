4f890c4d24de14ba8a4e344b9669600c113e231c
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-01-06 03:00:01 -0600

    adding uart_eg01.py

---
+++ uart-eg01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# uart-eg01.py
+#
+# to run on the other end of the UART
+# screen /dev/ttyUSB1 115200
+
+import serial
+
+def readlineCR(uart):
+	line = b''
+	while True:
+		byte = uart.read()
+		line += byte
+		if byte == b'\r':
+			return line
+
+uart = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=1)
+
+while True:
+	uart.write(b'\r\nSay something: ')
+	line = readlineCR(uart)
+	lineStr = '\r\nYou sent     : {}'.format(line.decode('utf-8'))
+	uart.write(lineStr.encode('utf-8'))
