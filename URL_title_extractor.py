345ee43dd67555cd7ccb604b1955aa49147d3e97
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-08-11 10:22:46 -0500

    Updated URL_title_extractor.py

--- URL_title_extractor.py
+++ URL_title_extractor.py
@@ -17,7 +17,7 @@ import html.parser, urllib.request
 
 class MyHTMLParser(html.parser.HTMLParser):
 	def __init__(self):
-		html.parser.HTMLParser.__init__(self)
+		html.parser.HTMLParser.__init__(self,convert_charrefs=True)
 		self.a = False
 		self.title = None
 		self.ogpTitle = None
@@ -38,25 +38,36 @@ class MyHTMLParser(html.parser.HTMLParser):
 			self.a = False
 			self.title = data
 
-parser = MyHTMLParser()
 
-try:
-	response = urllib.request.urlopen('http://instagram.com/sarah3llen', timeout=5)
-	contentType = response.getheader('Content-Type')
 
-	if 'text/html' in contentType:
-		data = response.read()
-		html = data.decode('utf-8')
+def main():
+	import sys
+	parser = MyHTMLParser()
 
-		parser.feed(html)
-
-		if parser.ogpTitle:
-			title = parser.ogpTitle
-		else:
-			title = parser.title
+	if sys.argv[1]:
+		url = sys.argv[1]
 	else:
-		title = None
-except:
-	title =  None
+		url = 'http://instagram.com/sarah3llen'
 
-print(title)
+	try:
+		response = urllib.request.urlopen(url, timeout=5)
+		contentType = response.getheader('Content-Type')
+
+		if 'text/html' in contentType:
+			data = response.read()
+			html = data.decode('utf-8')
+
+			parser.feed(html)
+
+			if parser.ogpTitle:
+				title = parser.ogpTitle
+			else:
+				title = parser.title
+		else:
+			title = None
+	except:
+		title =  None
+
+	print(title)
+
+if __name__ == "__main__": main()
