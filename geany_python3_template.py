2e3242acd41d1968da529915cca7e439e5a09151
Curtis Lee Bolin <CurtisLeeBolin@gmail.com>
2014-09-15 13:27:11 -0500

    Update geany_python3_template.py

--- geany_python3_template.py
+++ geany_python3_template.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 #
-{fileheader}
+# {filename}
+#
+# Copyright {year} {developer} <{mail}>
+#
 
 
 def main():
 
-	return 0
+    return 0
 
 if __name__ == '__main__':
-	main()
+    main()
+
+
